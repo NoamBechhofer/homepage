8dbebc45247e4fdae328684033f5b9a809200a43
NoamBechhofer <noam.bechhofer@gmail.com>
2025-12-09 18:41:52 -0500

    Update metadata for layout and resume pages

--- app/page.tsx
+++ app/page.tsx
@@ -4,10 +4,3 @@ import { redirect } from "next/navigation";
 export default function Home() {
   redirect("/resume");
 }
-
-export const metadata: Metadata = {
-  title: "Noam Bechhofer - Resume",
-  description: `Web developer with a passion for software. Quick learner and \
-great debugger. I am looking for an interesting position where I can grow and \
-learn.`,
-};
--- app/resume/page.tsx
+++ app/resume/page.tsx
@@ -1,5 +1,13 @@
+import { Metadata } from "next";
 import Resume from "./Resume";
 
 export default function Page() {
   return <Resume />;
 }
+
+export const metadata: Metadata = {
+  title: "Resume",
+  description: `Web developer with a passion for software. Quick learner and \
+great debugger. I am looking for an interesting position where I can grow and \
+learn.`,
+};
